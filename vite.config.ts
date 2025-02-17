c7b28e091f51ba41ef121a6f38f8c7ead695d8f0
Mazosolution <mazosolution37@gmail.com>
2025-02-17 19:17:23 +0530

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { componentTagger } from "lovable-tagger"
+
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: true,
+    port: 8080,
+    strictPort: true,
+  },
+  plugins: [
+    react(),
+    mode === 'development' && componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+  },
+  optimizeDeps: {
+    include: ['pdfjs-dist'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['pdfjs-dist/build/pdf.worker.min.mjs'],
+    },
+  },
+}))
